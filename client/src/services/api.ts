3ebfb30170feef61b6d398bc0aac4d1495b288d4
Katy Wells <katy.wells@verizon.net>
2026-03-10 12:01:44 -0400

    work on fetch filter, not yet finished

---
+++ client/src/services/api.ts
@@ -0,0 +1,21 @@
+/*
+API service functions - a layer to abstract API calls. 
+
+For example would contain functions like:
+
+getAllStrategies() -> moved from where it is now directly in the component
+
+createStrategy(data)
+
+updateStrategy(id, data)
+
+deleteStrategy(id)
+*/
+
+
+// Example structure
+export const fetchStrategies = (filters) => {
+  // Build query string from filters
+  // Return fetch promise
+}
+
